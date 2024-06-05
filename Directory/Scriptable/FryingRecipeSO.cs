623f3f1f4f8fe39adbe78ab51487f7277f94da95
anna1577623417 <129138975+anna1577623417@users.noreply.github.com>
2024-06-05 16:19:57 +0800

    Add files via upload

---
+++ Directory/Scriptable/FryingRecipeSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FryingRecipeSO : ScriptableObject {
+    public KitchenObjectSo input;
+    public KitchenObjectSo output;
+    public float fryingTimeMax;
+}
